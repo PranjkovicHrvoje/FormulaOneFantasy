e130ae08e6934e58a486d883c4a5b5ee4a6ea85b
hpranjkovic@gmail.com <Sifra041100>
2023-01-06 17:28:43 +0100

    Expanded API for races

--- app/src/main/java/com/example/formulaonefantasy/APIEndpoint.kt
+++ app/src/main/java/com/example/formulaonefantasy/APIEndpoint.kt
@@ -9,5 +9,8 @@ import retrofit2.http.Path
 interface APIEndpoint {
     @GET("/api/f1/drivers/{driver}.json")
     fun getDrivers(@Path("driver") driver: String): Call<ArrayList<Drivers>>
+
+    @GET("api/f1/current.json")
+    fun getRace(race: String): Call<ArrayList<Race>>
 }
 
