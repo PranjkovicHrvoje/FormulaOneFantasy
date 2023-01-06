c6edda935f6248999e0a8699ee1b28fbd24fa3ae
hpranjkovic@gmail.com <Sifra041100>
2023-01-06 17:32:52 +0100

    Race class adapter

---
+++ app/src/main/java/com/example/formulaonefantasy/RaceRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.formulaonefantasy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RaceRecyclerAdapter(val items: ArrayList<Race>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return RaceViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.season_schedule, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is RaceViewHolder -> {
+                holder.bind(items[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class RaceViewHolder(itemView: View):
+        RecyclerView.ViewHolder(itemView){
+        private val trackName = itemView.findViewById<TextView>(R.id.track_name)
+        private val raceDate = itemView.findViewById<TextView>(R.id.race_date)
+        private val raceLocation = itemView.findViewById<TextView>(R.id.race_location)
+
+        fun bind(race: Race){
+            trackName.text = race.circuitName
+            raceDate.text = race.date
+            raceLocation.text = race.Location.toString()
+        }
+    }
+}
