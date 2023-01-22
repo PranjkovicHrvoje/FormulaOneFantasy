b1b4ada1861730be73c2fc8021a585ba482fc04c
hpranjkovic@gmail.com <Sifra041100>
2023-01-22 20:54:56 +0100

    Player adapter

---
+++ app/src/main/java/com/example/formulaonefantasy/PlayerRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.formulaonefantasy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PlayerRecyclerAdapter(
+    val items: ArrayList<Player>
+
+    ):RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return PlayerViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.player_standings, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is PlayerViewHolder ->{
+                holder.bind(items[position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class PlayerViewHolder(itemView: View):
+        RecyclerView.ViewHolder(itemView){
+        private val player_Name = itemView.findViewById<TextView>(R.id.player_standings_nickname)
+        private val player_Points = itemView.findViewById<TextView>(R.id.player_standings_points)
+
+        fun bind(player: Player){
+            player_Name.text = player.nickname
+            player_Points.text = player.points.toString()
+        }
+    }
+
+}
