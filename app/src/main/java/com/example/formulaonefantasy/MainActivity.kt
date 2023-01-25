390dc99f950993c7b94842afdec30ef650d94e2e
hpranjkovic@gmail.com <Sifra041100>
2023-01-25 01:10:53 +0100

    Enabled switching between fragments

--- app/src/main/java/com/example/formulaonefantasy/MainActivity.kt
+++ app/src/main/java/com/example/formulaonefantasy/MainActivity.kt
@@ -2,99 +2,40 @@ package com.example.formulaonefantasy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageButton
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
+import com.example.formulaonefantasy.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private val db = Firebase.firestore
-    private lateinit var driverRecyclerAdapter: DriverRecyclerAdapter
-    private lateinit var raceRecyclerAdapter: RaceRecyclerAdapter
-    private lateinit var playerRecyclerAdapter: PlayerRecyclerAdapter
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(DriversFragment())
 
-        val recyclerView = findViewById<RecyclerView>(R.id.mainRecyclerView)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.select -> replaceFragment(DriversFragment())
+                R.id.calendar -> replaceFragment(ScheduleFragment())
+                R.id.standings -> replaceFragment(StandingsFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
 
-        val scheduleButton = findViewById<ImageButton>(R.id.calendarButton)
-        scheduleButton.setOnClickListener{
-            db.collection("races")
-                .get()
-                .addOnSuccessListener{
-                    val raceList: ArrayList<Race> = ArrayList()
-                    for(data in it.documents){
-                        val race = data.toObject(Race::class.java)
-                        if(race != null){
-                            race.id = data.id
-                            raceList.add(race)
-                        }
-                    }
-                    raceRecyclerAdapter = RaceRecyclerAdapter(raceList)
-                    recyclerView.apply {
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                        adapter = raceRecyclerAdapter
-                    }
-                }
-                .addOnFailureListener{
-                    Log.e("Error getting races.", it.message.toString() )
-                }
+                else ->{}
+            }
+            true
         }
+    }
 
-        val driverButton = findViewById<ImageButton>(R.id.inputDriverButton)
-        driverButton.setOnClickListener{
-            db.collection("drivers")
-                .get()
-                .addOnSuccessListener {
-                    val list : ArrayList<Drivers> = ArrayList()
-                    for (data in it.documents){
-                        val driver = data.toObject(Drivers::class.java)
-                        if(driver != null){
-                            driver.id = data.id
-                            list.add(driver)
-                        }
-                    }
-
-                    driverRecyclerAdapter = DriverRecyclerAdapter(list)
-                    recyclerView.apply{
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                        adapter = driverRecyclerAdapter
-                    }
-                }
-                .addOnFailureListener{
-                    Log.e("Error getting drivers.", it.message.toString())
-                }
-        }
-
-        val viewStandingsButton = findViewById<ImageButton>(R.id.standingsButton)
-        viewStandingsButton.setOnClickListener{
-            db.collection("players")
-                .get()
-                .addOnSuccessListener {
-                    val list : ArrayList<Player> = ArrayList()
-                    for(data in it.documents){
-                        val player = data.toObject(Player::class.java)
-                        if(player != null){
-                            player.id = data.id
-                            list.add(player)
-                        }
-                    }
-                    playerRecyclerAdapter = PlayerRecyclerAdapter(list)
-                    recyclerView.apply{
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                        adapter = playerRecyclerAdapter
-                    }
-                }
-                .addOnFailureListener{
-                    Log.e("Error getting standings.", it.message.toString())
-                }
-        }
-
-
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager=supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
