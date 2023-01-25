3ca3922e5286d79464dbf56b7a2533a58d781fea
hpranjkovic@gmail.com <Sifra041100>
2023-01-25 01:11:35 +0100

    Added favorite driver

--- app/src/main/java/com/example/formulaonefantasy/Player.kt
+++ app/src/main/java/com/example/formulaonefantasy/Player.kt
@@ -3,5 +3,6 @@ package com.example.formulaonefantasy
 data class Player(
     var id: String = "",
     val nickname: String? = null,
+    val favorite: String? = null,
     val points: Int? = null
 )
