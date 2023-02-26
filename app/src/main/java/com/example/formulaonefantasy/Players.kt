41dd828c147e31107befe12a7421a9f07303ade1
hpranjkovic@gmail.com <Sifra041100>
2023-02-26 18:24:18 +0100

    Changed val to var

--- app/src/main/java/com/example/formulaonefantasy/Players.kt
+++ app/src/main/java/com/example/formulaonefantasy/Players.kt
@@ -2,7 +2,8 @@ package com.example.formulaonefantasy
 
 data class Players(
     var id: String = "",
-    val nickname: String? = null,
+    var nickname: String? = null,
     var favorite: String? = null,
-    val points: Int? = null
+    var points: Int? = null,
+    var email: String? = null
 )
