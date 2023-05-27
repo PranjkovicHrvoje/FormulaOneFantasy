8e19984a8a666316549166b4b9b503a081814937
hpranjkovic@gmail.com <Sifra041100>
2023-05-27 21:01:51 +0200

    Fixed formatting

--- app/src/main/java/com/example/formulaonefantasy/Drivers.kt
+++ app/src/main/java/com/example/formulaonefantasy/Drivers.kt
@@ -2,8 +2,8 @@ package com.example.formulaonefantasy
 
 data class Drivers(
     var id: String = "",
-    val image: String? = null,
+    var image: String? = null,
     var name: String? = null,
-    val team: String? = null,
-    val country: String? = null
+    var team: String? = null,
+    var country: String? = null
 )
