ed5b69568e49b1edb15b20bd8ff8db0d793b068d
hpranjkovic@gmail.com <Sifra041100>
2023-05-27 21:03:41 +0200

    Changed datatype, fixed crashing

--- app/src/main/java/com/example/formulaonefantasy/Race.kt
+++ app/src/main/java/com/example/formulaonefantasy/Race.kt
@@ -1,10 +1,12 @@
 package com.example.formulaonefantasy
 
+import java.util.Date
+
 data class Race(
     var id: String = "",
+    val date: Date? = null,
     val name: String? = null,
-    val track: String? = null,
-    val date: String? = null
+    val track: String? = null
 )
 
 
